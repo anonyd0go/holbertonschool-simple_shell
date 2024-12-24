388ee0d564c453f1ed6fb2660bfcab583dc63bba
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-12-24 11:45:23 -0400

    Update prototype file

--- hshell.h
+++ hshell.h
@@ -6,8 +6,11 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 1024
+extern char **environ;
 
 /**
  * struct singly - Singly linked list for PATH
@@ -20,15 +23,25 @@ typedef struct singly
 	struct singly *next;
 } sll_t;
 
+/* Main Utils */
+char *shelline(void);
+char **getav(char *line);
+int execute(char **args);
+
 /* IO */
 ssize_t _getline(char **line, size_t *lnsz, FILE *stream);
 char *_strtok(char *str, const char *delim);
 
 /* Environment */
 char *_getenv(char *envar);
+/*PATH*/
 sll_t *list_path(sll_t **head, char *path);
 void free_sll(sll_t **head);
+size_t sll_size(sll_t *head);
 
 /* Helpers */
 size_t _strlen(char *str);
+char **realloc_args(char **args, size_t pos, size_t size);
+void freeargs(char **args);
+
 #endif
