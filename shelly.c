929078f5e6d24f5a38ac4144017ef8316ecb190e
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-12-26 11:50:57 -0400

    Correct shelly for betty

--- shelly.c
+++ shelly.c
@@ -6,38 +6,37 @@ volatile sig_atomic_t sigint_f = 0;
  */
 int main(void)
 {
-    char *line = NULL;
-    char **args;
-    int status = 1;
+	char *line = NULL;
+	char **args;
+	int status = 1;
 
-    signal(SIGINT, siginit_hndl);
-    do {
-        printf("(흫_흫):$ ");
-        fflush(stdout);
-        flag_prmpt(sigint_f);
-        line = shelline();
-        if (line == NULL)
-            continue;
-        
-        args = getav(line);
-        if (args == NULL)
-        {
-            free(line);
-            perror("Error: failed to get arguments");
-            status = 0;
-        }
-        if (is_exit(args))
-        {
-            free(line);
-            freeargs(args);
-            return (0);
-        }
-        status = execute(args);
+	signal(SIGINT, siginit_hndl);
+	do {
+		printf("(흫_흫):$ ");
+		fflush(stdout);
+		flag_prmpt(sigint_f);
+		line = shelline();
+		if (line == NULL)
+			continue;
+		args = getav(line);
+		if (args == NULL)
+		{
+			free(line);
+			perror("Error: failed to get arguments");
+			status = 0;
+		}
+		if (is_exit(args))
+		{
+			free(line);
+			freeargs(args);
+			return (0);
+		}
+		status = execute(args);
 
-        free(line);
-        freeargs(args);
-    } while (status);
-    return (0);
+		free(line);
+		freeargs(args);
+	} while (status);
+	return (0);
 }
 
 /**
@@ -46,6 +45,6 @@ int main(void)
  */
 void siginit_hndl(int sig)
 {
-    (void)sig;
-    sigint_f = 1;
+	(void)sig;
+	sigint_f = 1;
 }
