64b83573fce43d2e72e5a76ccb36330737769293
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-12-27 19:53:02 -0400

    Update custom strtok for betty style

--- sh_strtok.c
+++ sh_strtok.c
@@ -8,35 +8,35 @@
  */
 char *_strtok(char *str, const char *delim)
 {
-    static char *token_ptr = NULL;
-    char *start_tok = NULL;
-    char *end_tok = NULL;
+	static char *token_ptr;
+	char *start_tok = NULL;
+	char *end_tok = NULL;
 
-    if (str != NULL)
-        token_ptr = str;
-    if (token_ptr == NULL)
-        return (0);
+	if (str != NULL)
+		token_ptr = str;
+	if (token_ptr == NULL)
+		return (0);
 
-    start_tok = token_ptr;
-    while (*start_tok && strchr(delim, *start_tok))
-        start_tok++;
-    if (*start_tok == '\0')
-    {
-        token_ptr = NULL;
-        return (0);
-    }
+	start_tok = token_ptr;
+	while (*start_tok && strchr(delim, *start_tok))
+		start_tok++;
+	if (*start_tok == '\0')
+	{
+		token_ptr = NULL;
+		return (0);
+	}
 
-    end_tok = start_tok;
-    while (*end_tok && !strchr(delim, *end_tok))
-        end_tok++;
+	end_tok = start_tok;
+	while (*end_tok && !strchr(delim, *end_tok))
+		end_tok++;
 
-    if (*end_tok == '\0')
-        token_ptr = NULL;
-    else
-    {
-        *end_tok = '\0';
-        token_ptr = end_tok + 1;
-    }
+	if (*end_tok == '\0')
+		token_ptr = NULL;
+	else
+	{
+		*end_tok = '\0';
+		token_ptr = end_tok + 1;
+	}
 
-    return (start_tok);
+	return (start_tok);
 }
