9dec6acc6299a71ea3fcaf16a39e366e05bdf67c
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-12-26 21:50:32 -0400

    Update builtins for betty

--- built_in.c
+++ built_in.c
@@ -1,30 +1,30 @@
 #include "hshell.h"
 
 /**
- * execute_builtin - execute builtin commands
+ * exec_builtins - execute builtin commands
  * @args: arguments from command line
- * return: 1 if the command is builtin, 0 otherwise
+ * Return: 1 if the command is builtin, 0 otherwise
  */
 int exec_builtins(char **args)
 {
-    size_t i;
-    builtin_t builtins[] = {
-        {"env", is_env},
-        {"getenv", is_getenv},
-        {NULL, NULL}
-    };
+	size_t i;
+	builtin_t builtins[] = {
+		{"env", is_env},
+		{"getenv", is_getenv},
+		{NULL, NULL}
+	};
 
-    if (args == NULL || args[0] == NULL)
-        return (BI_FAIL);
-    for (i = 0; builtins[i].name != NULL; i++)
-    {
-        if (strcmp(args[0], builtins[i].name) == 0)
-        {
-            builtins[i].f(args);
-            return (BI_OK);
-        }
-    }
-    return (BI_FAIL);
+	if (args == NULL || args[0] == NULL)
+		return (BI_FAIL);
+	for (i = 0; builtins[i].name != NULL; i++)
+	{
+		if (strcmp(args[0], builtins[i].name) == 0)
+		{
+			builtins[i].f(args);
+			return (BI_OK);
+		}
+	}
+	return (BI_FAIL);
 }
 
 
@@ -35,11 +35,11 @@ int exec_builtins(char **args)
  */
 int is_exit(char **args)
 {
-    if (args == NULL || args[0] == NULL)
-        return (EXIT_FAIL);
-    if (strcmp(args[0], "exit") == 0)
-        return (EXIT_OK);
-    return (EXIT_FAIL);
+	if (args == NULL || args[0] == NULL)
+		return (EXIT_FAIL);
+	if (strcmp(args[0], "exit") == 0)
+		return (EXIT_OK);
+	return (EXIT_FAIL);
 }
 
 
@@ -50,16 +50,17 @@ int is_exit(char **args)
  */
 int is_env(char **args)
 {
-    size_t i;
-    if (args == NULL || args[0] == NULL)
-        return (BI_FAIL);
-    if (strcmp(args[0], "env") == 0)
-    {
-        for (i = 0; environ[i] != NULL; i++)
-            printf("%s\n", environ[i]);
-        return (BI_OK);
-    }
-    return (BI_FAIL);
+	size_t i;
+
+	if (args == NULL || args[0] == NULL)
+		return (BI_FAIL);
+	if (strcmp(args[0], "env") == 0)
+	{
+		for (i = 0; environ[i] != NULL; i++)
+			printf("%s\n", environ[i]);
+		return (BI_OK);
+	}
+	return (BI_FAIL);
 }
 
 
@@ -70,24 +71,25 @@ int is_env(char **args)
  */
 int is_getenv(char **args)
 {
-    char *env;
-    if (args == NULL || args[0] == NULL)
-        return (BI_FAIL);
-    if (strcmp(args[0], "getenv") == 0)
-    {
-        if (args[1] == NULL)
-        {
-            fprintf(stderr, "getenv: missing argument\n");
-            return (BI_OK);
-        }
-        env = _getenv(args[1]);
-        if (env == NULL)
-        {
-            fprintf(stderr, "getenv: %s: not found\n", args[1]);
-            return (BI_OK);
-        }
-        printf("%s\n", env);
-        return (BI_OK);
-    }
-    return (BI_FAIL);
+	char *env;
+
+	if (args == NULL || args[0] == NULL)
+		return (BI_FAIL);
+	if (strcmp(args[0], "getenv") == 0)
+	{
+		if (args[1] == NULL)
+		{
+			fprintf(stderr, "getenv: missing argument\n");
+			return (BI_OK);
+		}
+		env = _getenv(args[1]);
+		if (env == NULL)
+		{
+			fprintf(stderr, "getenv: %s: not found\n", args[1]);
+			return (BI_OK);
+		}
+		printf("%s\n", env);
+		return (BI_OK);
+	}
+	return (BI_FAIL);
 }
